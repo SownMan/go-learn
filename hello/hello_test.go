6faab00dc15b254b0e7a1b08cdd6e69364d3ca68
SownMan <rokizain26@gmail.com>
2023-01-14 12:35:58 +0700

    hello package testing go mod package management

---
+++ hello/hello_test.go
@@ -0,0 +1,14 @@
+package hello
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	got := Hello()
+	want := "Hello, world."
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
